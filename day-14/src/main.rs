ce3490d044e9177418ee2c7b269bc4e998b8d565
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2020-12-14 12:02:09 +0100

    minor performance improvements

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -13,14 +13,12 @@ fn main() {
     );
 }
 
-fn run_and_sum_values(
-    memory: &mut HashMap<u128, u128>,
-    data: &[(String, Vec<(u128, u128)>)],
-) -> u128 {
-    for (mask, instructions) in data {
+fn run_and_sum_values(memory: &mut HashMap<usize, usize>, data: &[Data]) -> usize {
+    for item in data {
+        let Data { mask, instructions } = item;
         for &(addr, new_val) in instructions {
             let mut val = new_val;
-            for (i, ch) in mask.chars().rev().enumerate() {
+            for (i, ch) in mask.iter().rev().enumerate() {
                 match ch {
                     '1' => val |= 1 << i,
                     '0' => val &= !(1 << i),
@@ -33,14 +31,13 @@ fn run_and_sum_values(
     memory.values().sum()
 }
 
-fn run_and_sum_values2(
-    memory: &mut HashMap<u128, u128>,
-    data: &[(String, Vec<(u128, u128)>)],
-) -> u128 {
-    for (mask, instructions) in data {
-        for &(addr, val) in instructions {
-            let addr_mask = generate_addr_mask(addr, &mask);
-            for actual_addr in generate_address_values(&addr_mask, addr_mask, 0) {
+fn run_and_sum_values2(memory: &mut HashMap<usize, usize>, data: &[Data]) -> usize {
+    for item in data {
+        for &(addr, val) in &item.instructions {
+            let (addr_mask, initial_val) = generate_addr_mask(addr, &item.mask);
+            let mut addresses = Vec::new();
+            generate_address_values(&addr_mask, initial_val, 0, &mut addresses);
+            for actual_addr in addresses {
                 memory.insert(actual_addr, val);
             }
         }
@@ -48,43 +45,43 @@ fn run_and_sum_values2(
     memory.values().sum()
 }
 
-fn generate_addr_mask(address: u128, mask: &str) -> [char; 36] {
+fn generate_addr_mask(address: usize, mask: &[char]) -> ([char; 36], usize) {
     let mut res = ['0'; 36];
-    for (addr_ch, ch) in format!("{:b}", address)
-        .chars()
-        .rev()
-        .zip(res.iter_mut().rev())
-    {
-        *ch = addr_ch;
-    }
-    for (mask_ch, ch) in mask.chars().zip(res.iter_mut()) {
+    let mut val = 0;
+    for ((i, mask_ch), ch) in mask.iter().rev().enumerate().zip(res.iter_mut().rev()) {
         match mask_ch {
-            '1' => *ch = '1',
-            'X' => *ch = 'X',
-            _ => {}
+            '1' => {
+                *ch = '1';
+                val |= 1 << i;
+            }
+            'X' => {
+                *ch = 'X';
+                val &= !(1 << i);
+            }
+            _ => val |= address & (1 << i),
         }
     }
-    res
+    (res, val)
 }
 
-fn generate_address_values(mask: &[char; 36], candidate: [char; 36], i: usize) -> Vec<u128> {
+fn generate_address_values(mask: &[char; 36], candidate: usize, i: usize, acc: &mut Vec<usize>) {
     if i == mask.len() {
-        return vec![candidate.iter().collect::<String>().parse().unwrap()];
-    }
-    if mask[i] != 'X' {
-        return generate_address_values(mask, candidate, i + 1);
+        acc.push(candidate);
+        return;
     }
 
-    let mut results = Vec::new();
-    let mut candidate_clone = candidate;
-    candidate_clone[i] = '0';
-    results.push(generate_address_values(mask, candidate_clone, i + 1));
-    candidate_clone[i] = '1';
-    results.push(generate_address_values(mask, candidate_clone, i + 1));
-    results.concat()
+    generate_address_values(mask, candidate, i + 1, acc);
+    if mask[i] == 'X' {
+        generate_address_values(mask, candidate | 1 << (mask.len() - 1 - i), i + 1, acc);
+    }
 }
 
-fn read(filename: &str) -> Vec<(String, Vec<(u128, u128)>)> {
+struct Data {
+    mask: Vec<char>,
+    instructions: Vec<(usize, usize)>,
+}
+
+fn read(filename: &str) -> Vec<Data> {
     read_to_string(filename)
         .expect("Failed to read file")
         .split_terminator("mask = ")
@@ -101,7 +98,10 @@ fn read(filename: &str) -> Vec<(String, Vec<(u128, u128)>)> {
                     Some((address.parse().unwrap(), parts[1].parse().unwrap()))
                 })
                 .collect();
-            (lines[0].into(), instructions)
+            Data {
+                mask: lines[0].chars().collect(),
+                instructions,
+            }
         })
         .collect()
 }
