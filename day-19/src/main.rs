987bded3f42bde185914edc80156e96aa26a1d3c
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2020-12-19 13:06:22 +0100

    improving validator to cover general cases

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -1,116 +1,74 @@
-use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
 fn main() {
-    let data = read("input.txt");
-    println!(
-        "part1 solution: {}",
-        count_matches(&data, &|&(count42, count31)| count42 == 2 && count31 == 1)
-    );
-
-    println!(
-        "part2 solution: {}",
-        count_matches(&data, &|&(count42, count31)| count31 > 0
-            && count42 > count31)
-    );
+    let mut data = read("input.txt");
+    println!("part1 solution: {}", count_matches(&data));
+    println!("part2 solution: {}", count_matches2(&mut data));
 }
 
-fn count_matches(
-    data: &(HashMap<usize, Vec<Value>>, Vec<String>),
-    filter_fn: &dyn Fn(&(usize, usize)) -> bool,
-) -> usize {
-    let (rules, messages) = data;
+fn count_matches(data: &(HashMap<usize, Vec<Value>>, Vec<String>)) -> usize {
     let mut memo = HashMap::new();
-    let possibilities_42 = generate_possibilities(&rules, &mut memo, 42);
-    let possibilities_31 = generate_possibilities(&rules, &mut memo, 31);
-
-    messages
+    data.1
         .iter()
-        .filter_map(|message| {
-            let mut s = message.get(..).unwrap();
-            let mut count42 = 0;
-            while let Some(pos_position) = possibilities_42.iter().position(|r| s.starts_with(r)) {
-                s = s.strip_prefix(&possibilities_42[pos_position]).unwrap();
-                count42 += 1;
-            }
-            if count42 == 0 {
-                return None;
-            }
-            let mut count31 = 0;
-            while let Some(pos_position) = possibilities_31.iter().position(|r| s.starts_with(r)) {
-                s = s.strip_prefix(&possibilities_31[pos_position]).unwrap();
-                count31 += 1;
-            }
-
-            if count31 == 0 || !s.is_empty() {
-                return None;
-            }
-            Some((count42, count31))
+        .cloned()
+        .filter(|msg| {
+            let hits = matches_rule(&msg, &data.0, 0, &mut memo);
+            !hits.is_empty() && hits.iter().any(|&i| i == msg.len())
         })
-        .filter(filter_fn)
         .count()
 }
 
-#[allow(dead_code)]
-fn count_matches_generated(data: &(HashMap<usize, Vec<Value>>, Vec<String>)) -> usize {
-    let (rules, messages) = data;
-    let mut memo = HashMap::new();
-    let possibilities = generate_possibilities(&rules, &mut memo, 0);
-
-    messages
-        .iter()
-        .filter(|message| possibilities.contains(message))
-        .count()
+fn count_matches2(data: &mut (HashMap<usize, Vec<Value>>, Vec<String>)) -> usize {
+    data.0
+        .insert(8, vec![Value::Rules(vec![42]), Value::Rules(vec![42, 8])]);
+    data.0.insert(
+        11,
+        vec![Value::Rules(vec![42, 31]), Value::Rules(vec![42, 11, 31])],
+    );
+    count_matches(data)
 }
 
-fn generate_possibilities(
-    rules_map: &HashMap<usize, Vec<Value>>,
-    memo: &mut HashMap<usize, Vec<String>>,
-    curr_rule: usize,
-) -> Vec<String> {
-    if let Some(values) = memo.get(&curr_rule) {
-        return values.clone();
-    }
-    let mut res: Vec<String> = Vec::new();
-    if let Some(rules) = rules_map.get(&curr_rule) {
-        match &rules[0] {
-            Value::Char(ch) => res.push(String::from(*ch)),
-            Value::Other(subrule) => {
-                let values = subrule
-                    .iter()
-                    .map(|r| generate_possibilities(rules_map, memo, *r))
-                    .collect::<Vec<_>>();
-                res.extend(values.iter().multi_cartesian_product().map(|product| {
-                    product.iter().fold(String::new(), |mut acc, curr| {
-                        acc.push_str(&curr);
-                        acc
-                    })
-                }));
-                if rules.len() > 1 {
-                    match &rules[1] {
-                        Value::Other(subrule2) => {
-                            let values = subrule2
-                                .iter()
-                                .map(|r| generate_possibilities(rules_map, memo, *r))
-                                .collect::<Vec<_>>();
-                            res.extend(values.iter().multi_cartesian_product().map(|product| {
-                                product.iter().fold(String::new(), |mut acc, curr| {
-                                    acc.push_str(&curr);
-                                    acc
-                                })
-                            }));
-                        }
-                        _ => {
-                            println!("char appeared when multiple rules");
-                        }
+fn matches_rule(
+    message: &str,
+    rules: &HashMap<usize, Vec<Value>>,
+    rule: usize,
+    memo: &mut HashMap<(String, usize), bool>,
+) -> Vec<usize> {
+    let matched_strings = match rules.get(&rule) {
+        Some(values) => values.iter().fold(Vec::new(), |mut acc, v| {
+            match &v {
+                Value::String(s) => {
+                    if message.starts_with(s) {
+                        acc.push(s.len());
                     }
                 }
-            }
-        }
-    }
-    memo.insert(curr_rule, res.clone());
-    res
+                Value::Rules(r_vec) => {
+                    let mut candidates = vec![(message, 0)];
+                    r_vec.iter().for_each(|r| {
+                        let mut new_candidates = Vec::new();
+                        for &(cand, cand_split_pos) in candidates.iter() {
+                            let matches = matches_rule(cand, rules, *r, memo);
+                            if matches.is_empty() {
+                                continue;
+                            }
+                            new_candidates.extend(matches.iter().map(|&r| {
+                                let (_, remaining) = cand.split_at(r);
+                                (remaining, cand_split_pos + r)
+                            }));
+                        }
+                        candidates = new_candidates
+                    });
+                    if !candidates.is_empty() {
+                        acc.extend(candidates.iter().map(|&(_s, res)| res));
+                    }
+                }
+            };
+            acc
+        }),
+        None => Vec::new(),
+    };
+    matched_strings
 }
 
 fn read(filename: &str) -> (HashMap<usize, Vec<Value>>, Vec<String>) {
@@ -125,9 +83,9 @@ fn read(filename: &str) -> (HashMap<usize, Vec<Value>>, Vec<String>) {
                 .map(|r| {
                     let v: Value;
                     if r.starts_with('"') {
-                        v = Value::Char(r.chars().nth(1).unwrap());
+                        v = Value::String(r.get(1..2).unwrap().to_string());
                     } else {
-                        v = Value::Other(
+                        v = Value::Rules(
                             r.split_whitespace()
                                 .filter_map(|val| val.parse().ok())
                                 .collect(),
@@ -139,15 +97,15 @@ fn read(filename: &str) -> (HashMap<usize, Vec<Value>>, Vec<String>) {
             (items[0].parse().unwrap(), rules)
         })
         .collect();
-    let messages = parts[1].lines().map(|l| l.chars().collect()).collect();
+    let messages = parts[1].lines().map(|l| l.to_string()).collect();
 
     (rules, messages)
 }
 
 #[derive(Debug, Clone)]
 enum Value {
-    Char(char),
-    Other(Vec<usize>),
+    String(String),
+    Rules(Vec<usize>),
 }
 
 #[cfg(test)]
@@ -157,21 +115,13 @@ mod tests {
     #[test]
     fn test1() {
         let data = read("test-input.txt");
-        assert_eq!(count_matches_generated(&data), 2);
+        assert_eq!(count_matches(&data), 2);
     }
 
     #[test]
     fn test2() {
-        let data = read("test-input2.txt");
-        assert_eq!(
-            count_matches(&data, &|&(count42, count31)| count42 == 2 && count31 == 1),
-            3
-        );
-
-        assert_eq!(
-            count_matches(&data, &|&(count42, count31)| count31 > 0
-                && count42 > count31),
-            12
-        );
+        let mut data = read("test-input2.txt");
+        assert_eq!(count_matches(&data), 3);
+        assert_eq!(count_matches2(&mut data), 12);
     }
 }
