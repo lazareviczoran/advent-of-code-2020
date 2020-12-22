66bf3c80cc1a419de44f580e2d019b37a3b757c5
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2020-12-22 08:24:54 +0100

    day 22

---
+++ day-22/src/main.rs
@@ -0,0 +1,144 @@
+use std::collections::{HashSet, VecDeque};
+use std::fs::read_to_string;
+
+fn main() {
+    let mut game = read("input.txt");
+    game.play_simple();
+    println!("part1 solution: {:?}", game.calculate_score());
+
+    game.reset();
+    game.play_recursive();
+    println!("part2 solution: {}", game.calculate_score());
+}
+
+#[derive(Debug, Clone)]
+struct Game {
+    decks: [VecDeque<usize>; 2],
+    prev_states: HashSet<u128>,
+    curr_state: u128,
+    initial_decks: [VecDeque<usize>; 2],
+    initial_state: u128,
+    winner: usize,
+}
+impl Game {
+    pub fn new(decks: [VecDeque<usize>; 2]) -> Self {
+        let initial_decks = decks.clone();
+        let mut curr_state = 0;
+        for (val1, val2) in decks[0].iter().zip(decks[1].iter()) {
+            curr_state |= 1 << val1;
+            curr_state |= 1 << val2 << 64;
+        }
+        let initial_state = curr_state;
+        Self {
+            decks,
+            initial_decks,
+            prev_states: HashSet::new(),
+            curr_state,
+            initial_state,
+            winner: usize::MAX,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.decks = self.initial_decks.clone();
+        self.curr_state = self.initial_state;
+        self.prev_states.clear();
+        self.winner = usize::MAX;
+    }
+
+    pub fn play_simple(&mut self) {
+        while !self.decks[0].is_empty() && !self.decks[1].is_empty() {
+            let (curr1, curr2) = self.draw_top_cards();
+            self.append_to_winner((curr1 < curr2) as usize, curr1, curr2);
+        }
+        self.winner = self.decks[0].is_empty() as usize;
+    }
+
+    pub fn play_recursive(&mut self) {
+        while !self.decks[0].is_empty() && !self.decks[1].is_empty() {
+            if self.prev_states.contains(&self.curr_state) {
+                break;
+            }
+            self.save_state();
+            let (curr1, curr2) = self.draw_top_cards();
+            if curr1 <= self.decks[0].len() && curr2 <= self.decks[1].len() {
+                let mut new_game = self.create_subgame(curr1, curr2);
+                new_game.play_recursive();
+                self.append_to_winner(new_game.winner, curr1, curr2);
+            } else {
+                self.append_to_winner((curr1 < curr2) as usize, curr1, curr2);
+            }
+        }
+        self.winner = self.decks[0].is_empty() as usize;
+    }
+
+    pub fn calculate_score(&self) -> usize {
+        self.decks[self.winner]
+            .iter()
+            .rev()
+            .enumerate()
+            .fold(0, |acc, (i, v)| acc + (i + 1) * *v)
+    }
+
+    pub fn create_subgame(&mut self, take1: usize, take2: usize) -> Self {
+        let new_deck1 = self.decks[0].iter().take(take1).copied().collect();
+        let new_deck2 = self.decks[1].iter().take(take2).copied().collect();
+        Game::new([new_deck1, new_deck2])
+    }
+
+    pub fn draw_top_cards(&mut self) -> (usize, usize) {
+        let (curr1, curr2) = (
+            self.decks[0].pop_front().unwrap(),
+            self.decks[1].pop_front().unwrap(),
+        );
+        self.curr_state &= !(1 << curr1);
+        self.curr_state &= !(1 << curr2 << 64);
+        (curr1, curr2)
+    }
+
+    pub fn append_to_winner(&mut self, winner: usize, val1: usize, val2: usize) {
+        self.decks[winner].push_back(if winner == 0 { val1 } else { val2 });
+        self.decks[winner].push_back(if winner == 0 { val2 } else { val1 });
+        self.curr_state |= 1 << val1 << (64 * winner);
+        self.curr_state |= 1 << val2 << (64 * winner);
+    }
+
+    pub fn save_state(&mut self) {
+        self.prev_states.insert(self.curr_state);
+    }
+}
+
+fn read(filename: &str) -> Game {
+    let content = read_to_string(filename).expect("Failed to read file");
+    let mut cards = content
+        .split_terminator("\n\n")
+        .map(|s| s.lines().skip(1).filter_map(|l| l.parse().ok()).collect());
+    let decks = [cards.next().unwrap(), cards.next().unwrap()];
+    Game::new(decks)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut game = read("test-input.txt");
+        game.play_simple();
+        assert_eq!(game.calculate_score(), 306);
+
+        game.reset();
+        game.play_recursive();
+        assert_eq!(game.calculate_score(), 291);
+    }
+
+    #[test]
+    fn test_infinite() {
+        let mut game = Game::new([
+            VecDeque::from(vec![43, 19]),
+            VecDeque::from(vec![2, 29, 14]),
+        ]);
+        game.play_recursive();
+        assert_eq!(game.winner, 0);
+    }
+}
